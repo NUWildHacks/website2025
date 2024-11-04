3c0cab2338133909f965697bdfceeaaa9c5f7c8a
Andrew Li <andrewli@u.northwestern.edu>
2024-11-03 19:41:27 -0600

    some boilerplate code

---
+++ src/components/SampleComponent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Colors } from "../globals";
+
+export const SampleComponent: React.FC = () => {
+    return (
+        <p style={{ color: Colors.Blue }}>Hello World</p>
+    );
+};
