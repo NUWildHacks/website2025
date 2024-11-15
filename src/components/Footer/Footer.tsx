124e778b8118182675f63b332732ad0bc33a7cc1
Andrew Li <andrewli@u.northwestern.edu>
2024-11-15 16:59:16 -0600

    update footer styles to be mobile adaptive

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,12 +7,19 @@ interface IFooter {}
 export const Footer: React.FC<IFooter> = () => {
   return (
     <div id="footer">
-      <img src={reactLogo} alt="Placeholder" />
-      <h2>WildHacks 2025</h2>
-      <div id="footer-socials">
-        <a href="https://www.instagram.com/wildhacks/">Instagram</a>
-        <a href="https://www.linkedin.com/company/wildhacks/">LinkedIn</a>
-        <a href="mailto:wildhacks@northwestern.edu">Email</a>
+      <div className="footer__flexbox">
+        <img src={reactLogo} alt="Placeholder" />
+        <h2>WildHacks 2025</h2>
+      </div>
+      <div className="footer__flexbox">
+        Something should go here eventually
+      </div>
+      <div>
+        <div className="footer__flexbox">
+          <a href="https://www.instagram.com/wildhacks/">Instagram</a>
+          <a href="https://www.linkedin.com/company/wildhacks/">LinkedIn</a>
+          <a href="mailto:wildhacks@northwestern.edu">Email</a>
+        </div>
       </div>
     </div>
   );
