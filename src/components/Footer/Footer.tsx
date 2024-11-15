77d5a4859504e24191481c73f4f0308c48d1d8fd
Andrew Li <andrewli@u.northwestern.edu>
2024-11-15 17:06:07 -0600

    Footer - Darin (new PR) (#3)
    * implement footer component with layout styling; use placeholder react logo
    
    * apply consistent formatting
    
    * update footer styles to be mobile adaptive
    
    * fix dependabot vulnerability
    
    * formatting
    
    ---------
    
    Co-authored-by: Darin <darincheng2026@u.northwestern.edu>

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import reactLogo from "../../assets/react.svg";
 import "./Footer.scss";
 
 interface IFooter {}
 
 export const Footer: React.FC<IFooter> = () => {
-  return <div>some words</div>;
+  return (
+    <div id="footer">
+      <div className="footer__flexbox">
+        <img src={reactLogo} alt="Placeholder" />
+        <h2>WildHacks 2025</h2>
+      </div>
+      <div className="footer__flexbox">Something should go here eventually</div>
+      <div>
+        <div className="footer__flexbox">
+          <a href="https://www.instagram.com/wildhacks/">Instagram</a>
+          <a href="https://www.linkedin.com/company/wildhacks/">LinkedIn</a>
+          <a href="mailto:wildhacks@northwestern.edu">Email</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
