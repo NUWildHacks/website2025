41b48aa720a1cad1579e90339276e2afaa9925c8
dweber2022 <102439573+dweber2022@users.noreply.github.com>
2024-11-15 17:05:37 -0600

    Implement footer component with layout styling (#2)
    * implement footer component with layout styling; use placeholder react logo
    
    * apply consistent formatting
    
    ---------
    
    Co-authored-by: Darin <darincheng2026@u.northwestern.edu>

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import reactLogo from "../../assets/react.svg";
 import "./Footer.scss";
 
 interface IFooter {}
 
 export const Footer: React.FC<IFooter> = () => {
-  return <div>some words</div>;
+  return (
+    <div id="footer">
+      <img src={reactLogo} alt="Placeholder" />
+      <h2>WildHacks 2025</h2>
+      <div id="footer-socials">
+        <a href="https://www.instagram.com/wildhacks/">Instagram</a>
+        <a href="https://www.linkedin.com/company/wildhacks/">LinkedIn</a>
+        <a href="mailto:wildhacks@northwestern.edu">Email</a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
