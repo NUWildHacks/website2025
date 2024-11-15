8001cc37dc08c8c1943465acbbf5645e42e8672a
Darin <darincheng2026@u.northwestern.edu>
2024-11-15 12:39:45 -0600

    apply consistent formatting

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,15 +5,17 @@ import "./Footer.scss";
 interface IFooter {}
 
 export const Footer: React.FC<IFooter> = () => {
-  return (<div id="footer">
-    <img src={ reactLogo } alt="Placeholder" />
-    <h2>WildHacks 2025</h2>
-    <div id="footer-socials">
-      <a href="https://www.instagram.com/wildhacks/">Instagram</a>
-      <a href="https://www.linkedin.com/company/wildhacks/">LinkedIn</a>
-      <a href="mailto:wildhacks@northwestern.edu">Email</a>
+  return (
+    <div id="footer">
+      <img src={reactLogo} alt="Placeholder" />
+      <h2>WildHacks 2025</h2>
+      <div id="footer-socials">
+        <a href="https://www.instagram.com/wildhacks/">Instagram</a>
+        <a href="https://www.linkedin.com/company/wildhacks/">LinkedIn</a>
+        <a href="mailto:wildhacks@northwestern.edu">Email</a>
+      </div>
     </div>
-  </div>);
+  );
 };
 
 export default Footer;
