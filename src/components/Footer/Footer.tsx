a18062a1453d2f980ad2122e40ec686c7a1c902e
Andrew Li <andrewli@u.northwestern.edu>
2024-11-17 09:18:23 -0600

    Add Font Awesome, add noreferrer to anchors, use logo icons instead of words for socials, add Wildhacks logo to footer (#6)
    * add noreferrer to anchor elements
    
    * add FontAwesome; use logo icons instead of words for socials
    
    * add Wildhacks logo and change color of FontAwesome icons
    
    ---------
    
    Co-authored-by: Darin <darincheng2026@u.northwestern.edu>

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import reactLogo from "../../assets/react.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import wildhacksLogo from "../../assets/wildhacks-logo-square.svg";
 import "./Footer.scss";
 
 interface IFooter {}
@@ -8,15 +11,26 @@ export const Footer: React.FC<IFooter> = () => {
   return (
     <div id="footer">
       <div className="footer__flexbox">
-        <img src={reactLogo} alt="Placeholder" />
+        <div className="footer__image">
+          <img src={wildhacksLogo} alt="Wildhacks 2025 logo" />
+        </div>
         <h2>WildHacks 2025</h2>
       </div>
       <div className="footer__flexbox">Something should go here eventually</div>
       <div>
-        <div className="footer__flexbox">
-          <a href="https://www.instagram.com/wildhacks/">Instagram</a>
-          <a href="https://www.linkedin.com/company/wildhacks/">LinkedIn</a>
-          <a href="mailto:wildhacks@northwestern.edu">Email</a>
+        <div className="footer__flexbox" id="icons__flexbox">
+          <a href="https://www.instagram.com/wildhacks/" rel="noreferrer">
+            <FontAwesomeIcon icon={faInstagram} size="2xl" />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/wildhacks/"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="2xl" />
+          </a>
+          <a href="mailto:wildhacks@northwestern.edu">
+            <FontAwesomeIcon icon={faEnvelope} size="2xl" />
+          </a>
         </div>
       </div>
     </div>
