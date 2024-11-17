55d5d02dcb380fdbb96ee4e49da81d4548398f90
Andrew Li <andrewli@u.northwestern.edu>
2024-11-17 09:17:20 -0600

    add Wildhacks logo and change color of FontAwesome icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import reactLogo from "../../assets/react.svg";
+import wildhacksLogo from "../../assets/wildhacks-logo-square.svg";
 import "./Footer.scss";
 
 interface IFooter {}
@@ -11,23 +11,25 @@ export const Footer: React.FC<IFooter> = () => {
   return (
     <div id="footer">
       <div className="footer__flexbox">
-        <img src={reactLogo} alt="Placeholder" />
+        <div className="footer__image">
+          <img src={wildhacksLogo} alt="Wildhacks 2025 logo" />
+        </div>
         <h2>WildHacks 2025</h2>
       </div>
       <div className="footer__flexbox">Something should go here eventually</div>
       <div>
-        <div className="footer__flexbox">
+        <div className="footer__flexbox" id="icons__flexbox">
           <a href="https://www.instagram.com/wildhacks/" rel="noreferrer">
-            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon icon={faInstagram} size="2xl" />
           </a>
           <a
             href="https://www.linkedin.com/company/wildhacks/"
             rel="noreferrer"
           >
-            <FontAwesomeIcon icon={faLinkedin} />
+            <FontAwesomeIcon icon={faLinkedin} size="2xl" />
           </a>
           <a href="mailto:wildhacks@northwestern.edu">
-            <FontAwesomeIcon icon={faEnvelope} />
+            <FontAwesomeIcon icon={faEnvelope} size="2xl" />
           </a>
         </div>
       </div>
