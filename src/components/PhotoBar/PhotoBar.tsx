20c3fce80b524f51a28b54a1085db45097a71a2c
Darin <darincheng2026@u.northwestern.edu>
2024-11-22 17:05:16 -0600

    add scrolling animation (currently hardcoded duration and rotation)

--- src/components/PhotoBar/PhotoBar.tsx
+++ src/components/PhotoBar/PhotoBar.tsx
@@ -5,17 +5,22 @@ interface IPhotoBar {}
 export const PhotoBar: React.FC<IPhotoBar> = () => {
   return (
     <div id="photobar">
-    { [...Array(50)].map((_, i) => 
-      <div key={i} className="photobar__flexbox">
-        <div className="photobar__image">
-          <img className="photo" src="https://brandenghena.com/images/branden_crossed.jpg" />
-        </div>
-        <p>Branden Ghena</p>
-      </div>)
-    }
+      <div className="photobar__animation">
+        {[...Array(50)].map((_, i) => (
+          <div key={i} className="photobar__flexbox">
+            <div className="photobar__image">
+              <img
+                className="photo"
+                src="https://brandenghena.com/images/branden_crossed.jpg"
+              />
+            </div>
+            <p>Branden Ghena</p>
+            <p>Professor</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-
 };
 
 export default PhotoBar;
