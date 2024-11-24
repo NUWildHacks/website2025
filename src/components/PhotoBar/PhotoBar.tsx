e32bbb1a4e833caae2b4de202c090b6d797b5cc0
Darin <darincheng2026@u.northwestern.edu>
2024-11-23 21:33:22 -0600

    add header for photo bar; make photos hyperlinks; import and loop over people for photos

--- src/components/PhotoBar/PhotoBar.tsx
+++ src/components/PhotoBar/PhotoBar.tsx
@@ -1,22 +1,30 @@
 import "./PhotoBar.scss";
+import { IPerson } from "./People";
+import People from "./People";
 
 interface IPhotoBar {}
 
 export const PhotoBar: React.FC<IPhotoBar> = () => {
   return (
     <div id="photobar">
+      <h2>Meet our Team</h2>
       <div className="photobar__animation">
-        {[...Array(50)].map((_, i) => (
-          <div key={i} className="photobar__flexbox">
-            <div className="photobar__image">
-              <img
-                className="photo"
-                src="https://brandenghena.com/images/branden_crossed.jpg"
-              />
+        {People.map((person: IPerson, i) => (
+          <a
+            href={person.link ? person.link : "javascript:void(0);"}
+            rel="noreferrer"
+          >
+            <div
+              key={i}
+              className={`photobar__flexbox ${i % 2 === 0 ? "photobar__rotate_right" : "photobar__rotate_left"}`}
+            >
+              <div className="photobar__image">
+                <img className="photo" src={person.img} />
+              </div>
+              <h3>{person.name}</h3>
+              <p>{person.position}</p>
             </div>
-            <p>Branden Ghena</p>
-            <p>Professor</p>
-          </div>
+          </a>
         ))}
       </div>
     </div>
