627ad73ec8b4cd17cffe3a1732888c012bcfe06d
Wendy Huang <wenjinghuang26@gmail.com>
2024-12-05 17:18:04 -0600

    nav bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,16 @@ import "./Header.scss";
 interface IHeader {}
 
 export const Header: React.FC<IHeader> = () => {
-  return <div className="header__box">some words</div>;
+  return (
+    <div className="header__box">
+      <div className="header__nav">
+        <button className="header__button">About</button>
+        <button className="header__button">FAQ</button>
+        <button className="header__button">Sponsors</button>
+        <button className="header__d_button">Dashboard</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
