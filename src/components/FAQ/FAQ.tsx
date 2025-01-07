efec13bf963b2ca049493b894804cb448c27a5f9
Andrew Li <andrewli@u.northwestern.edu>
2025-01-06 20:10:38 -0600

    first preliminary draft assets added

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -1,25 +1,53 @@
 import QuestionList from "./QuestionList/QuestionList";
 import QUESTION_LIST from "../../shared/questions";
 
-import tree from "../../assets/bg/tree.svg";
-
 import "./FAQ.scss";
+import { useOs } from "@mantine/hooks";
 
 interface IFAQ {}
 
 export const FAQ: React.FC<IFAQ> = () => {
-  return (
-    <div className="faq__wrapper">
-      <img src={tree} />
-      <div className="faq__header">Frequently Asked Questions</div>
+  const os = useOs();
 
-      {QUESTION_LIST.map((elem, index) => (
-        <QuestionList
-          header={elem.header}
-          questions={elem.questions}
-          key={`questionlist__${index}`}
-        />
-      ))}
+  // on mobile, no tree or buttons; no space.
+  if (os === "ios" || os === "android") {
+    return (
+      <div style={{display: "block"}}>
+        <div className="faq__wrapper">
+          <div className="faq__right_side">
+            <div className="faq__header">
+              Frequently Asked Questions
+            </div>
+            {QUESTION_LIST.map((elem, index) => (
+              <QuestionList
+                header={elem.header}
+                questions={elem.questions}
+                key={`questionlist__${index}`}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  // on non-mobile, add buttons to switch category
+  return (
+    <div>
+      <div className="faq__wrapper">
+        <div className="faq__right_side">
+          <div className="faq__header">
+            Frequently Asked Questions
+          </div>
+          {QUESTION_LIST.map((elem, index) => (
+            <QuestionList
+              header={elem.header}
+              questions={elem.questions}
+              key={`questionlist__${index}`}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -1,15 +1,32 @@
+import tree from "../../assets/bg/tree.svg";
+import nucsLogo from "../../assets/sponsors/nucs.png"
+import garageLogo from "../../assets/sponsors/the-garage.png"
+import omnistackLogo from "../../assets/sponsors/omnistack.png"
+import googleLogo from "../../assets/sponsors/google.png"
+
 import "./Sponsors.scss";
 
 interface ISponsors {}
 
-export const Sponsors: React.FC<ISponsors> = () => {
+export const Sponsors: React.FC<ISponsors> = () => {  
   return (
     <>
       <div className="sponsors__backdrop">
+        <div className="sponsors__tree">
+          <img src={tree} />
+        </div>
+
         <div className="sponsors__inner_wrapper">
           <div className="sponsors__header">Thank You To Our Sponsors</div>
-
+          <div className="sponsors__flexbox">
+            <div><img src={nucsLogo} /></div>
+            <div><img src={omnistackLogo} /></div>
+            <div><img src={garageLogo} /></div>
+          </div>
           <div className="sponsors__subheader">and Our Partners</div>
+          <div className="sponsors__flexbox">
+            <div><img src={googleLogo} /></div>
+          </div>
         </div>
       </div>
       {/* <div className="bottom_border" /> */}
