73bae264d6e447ceadde805a218d38dd9a963d61
dweber2022 <102439573+dweber2022@users.noreply.github.com>
2025-01-12 15:22:41 -0600

    use WaveVector in Sponsors component to hide bottom of tree; adjust Sponsors bottom padding and tree styling

--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -4,16 +4,27 @@ import garageLogo from "../../assets/sponsors/the-garage.png";
 import omnistackLogo from "../../assets/sponsors/omnistack.png";
 import googleLogo from "../../assets/sponsors/google.png";
 
+import WaveVector from "../WaveVector/WaveVector";
+import { colors } from "../../shared/colors.ts";
+
 import "./Sponsors.scss";
 
 interface ISponsors {}
 
 export const Sponsors: React.FC<ISponsors> = () => {
+  const TRANSPARENT = "#00000000";
   return (
     <>
       <div className="sponsors__backdrop">
         <div className="sponsors__tree">
           <img src={tree} />
+        </div>
+        <div id="sponsors__wave">
+          <WaveVector
+            color={colors.very_dark_green}
+            otherColor={TRANSPARENT}
+            waviness={0}
+          />
         </div>
 
         <div className="sponsors__inner_wrapper">
@@ -37,7 +48,6 @@ export const Sponsors: React.FC<ISponsors> = () => {
           </div>
         </div>
       </div>
-      {/* <div className="bottom_border" /> */}
     </>
   );
 };
