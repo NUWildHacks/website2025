f32be0611f17f2a6f83a01576c3e7cdd615b3de0
Darin <darincheng2026@u.northwestern.edu>
2025-01-11 18:21:08 -0600

    add form fields and submit event callback

--- src/components/EmailList/EmailList.tsx
+++ src/components/EmailList/EmailList.tsx
@@ -7,7 +7,10 @@ interface IEmailList {}
 
 export const EmailList: React.FC<IEmailList> = () => {
   const [opened, { open, close }] = useDisclosure(false);
-
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data: FormData = new FormData(e.target as HTMLFormElement);
+  };
   return (
     <div className="email_list__container">
       <Modal
@@ -17,7 +20,17 @@ export const EmailList: React.FC<IEmailList> = () => {
         zIndex={10000}
         centered
       >
-        <div className="email_form__container"></div>
+        <div className="email_form__container">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" name="firstName" id="firstName" required />
+            <label htmlFor="lastName Name">Last name</label>
+            <input type="text" name="lastName" id="lastName" required />
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" required />
+            <button type="submit">Register</button>
+          </form>
+        </div>
       </Modal>
       <button id="email_list__button" onClick={open}>
         Join the mailing list!
