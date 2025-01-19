f2ae5abdfc13cad470e18d2bfb461d322244f602
Andrew Li <andrewli@u.northwestern.edu>
2025-01-19 14:17:52 -0600

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/website2025/",
   plugins: [react()],
   css: {
     preprocessorOptions: {
