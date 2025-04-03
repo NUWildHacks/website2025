a13069f37b91923fbab1c0397375c0c8f61645dc
Andrew Li <andrewli@u.northwestern.edu>
2025-04-02 22:59:21 -0500

    add logos for other sponsors and partners to website

--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -1,19 +1,38 @@
 import tree from "../../assets/bg/tree.svg";
-import nucsLogo from "../../assets/sponsors/nucs.png";
-import garageLogo from "../../assets/sponsors/the-garage.png";
-import omnistackLogo from "../../assets/sponsors/omnistack.png";
 import googleLogo from "../../assets/sponsors/google.png";
+import nucsLogo from "../../assets/sponsors/nucs.png";
+import omnistackLogo from "../../assets/sponsors/omnistack.png";
 import stickersLogo from "../../assets/sponsors/stand-out-stickers.svg";
+import redBullLogo from "../../assets/sponsors/redbull.svg";
+import balsamiqLogo from "../../assets/sponsors/balsamiq-logo.svg";
+import hedgyLogo from "../../assets/sponsors/hedgy-logo.svg";
+import garageLogo from "../../assets/sponsors/the-garage.png";
+import geicoLogo from "../../assets/sponsors/geico.svg";
 
-import WaveVector from "../WaveVector/WaveVector";
 import { colors } from "../../shared/colors.ts";
+import WaveVector from "../WaveVector/WaveVector";
 
 import "./Sponsors.scss";
 
-interface ISponsors {}
+interface ISponsor {
+  altLabel: string,
+  src: string,
+  url?: string,
+};
 
-export const Sponsors: React.FC<ISponsors> = () => {
-  const TRANSPARENT = "#00000000";
+const SponsorImage: React.FC<ISponsor> = ({altLabel, src, url}) => {
+  return (
+    <a href={url}>
+      <div>
+        <img src={src} alt={altLabel} />
+      </div>
+    </a>
+  );
+}
+
+const TRANSPARENT = "#00000000";
+
+export const Sponsors: React.FC = () => {
   return (
     <>
       <div className="sponsors__backdrop">
@@ -31,26 +50,19 @@ export const Sponsors: React.FC<ISponsors> = () => {
         <div className="sponsors__inner_wrapper">
           <div className="sponsors__header">Thank You To Our Sponsors</div>
           <div className="sponsors__flexbox">
-            <div>
-              <img src={nucsLogo} />
-            </div>
-            <div>
-              <img src={omnistackLogo} />
-            </div>
-            <div>
-              <img src={garageLogo} />
-            </div>
+            <SponsorImage altLabel="Northwestern Department of Computer Science" src={nucsLogo} url="https://www.mccormick.northwestern.edu/computer-science/" />
+            <SponsorImage altLabel="OmniStack" src={omnistackLogo} />
+            <SponsorImage altLabel="The Garage @ Northwestern" src={garageLogo} url="https://www.thegarage.northwestern.edu/" />
+            <SponsorImage altLabel="Geico" src={geicoLogo} />
           </div>
           <div className="sponsors__subheader">and Our Partners</div>
           <div className="sponsors__flexbox">
-            <div>
-              <img src={googleLogo} />
-            </div>
-            <a href="https://www.standoutstickers.com/?utm_campaign=events-league-organizers-fall2023&utm_medium=email&utm_source=customerio-zoho_creator_-_standout_sticker_intro">
-              <div>
-                <img src={stickersLogo} />
-              </div>
-            </a>
+            <SponsorImage altLabel="Google" src={googleLogo} />
+            <SponsorImage altLabel="Standout Stickers" src={stickersLogo} url="https://www.standoutstickers.com/?utm_campaign=events-league-organizers-fall2023&utm_medium=email&utm_source=customerio-zoho_creator_-_standout_sticker_intro" />
+            {/* hedgy partner, red bull partner, balsamiq partner,  doordash partner */}
+            <SponsorImage altLabel="Hedgy" src={hedgyLogo} url="https://www.hedgy.works/" />
+            <SponsorImage altLabel="Red Bull" src={redBullLogo} />
+            <SponsorImage altLabel="Balsamiq" src={balsamiqLogo} url="https://balsamiq.com/" />
           </div>
         </div>
       </div>
